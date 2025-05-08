046415ced1a6e68b09ea587a022e16edaf1fab71
Gaindu Amarasinghe <gaindu2k03@gmail.com>
2025-05-08 18:15:44 +0530

    task 2 commented

--- src/pack/learner/basic/task2/NumberGuess.java
+++ src/pack/learner/basic/task2/NumberGuess.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class NumberGuess {
     public static void main(String[] args){
+
 //        Generating the random number between 1 and 20.
         Random random = new Random();
         int randomNumber = random.nextInt(20) + 1;
@@ -16,21 +17,23 @@ public class NumberGuess {
 
         Scanner input = new Scanner(System.in);
 
-//        This
+//        until complete 5 attempts user can enter numbers.
         while(attempts <= 5){
+//            Handle the user input using try-block.
             try{
-
                 System.out.print("""
                 ----------Guessing Game----------
                 ---------------------------------
                 """);
                 System.out.print("Guess the number: ");
+//                Getting user input.
                 int guessed_num = input.nextInt();
+//                validate the user input between 1 anc 20.
                 if (guessed_num < 0 || guessed_num > 20){
                     System.out.println("Error: 1 to 20 Integers only.");
                     continue;
                 }
-
+//                after user wins break the loop and finish the game
                 hasWon = result(guessed_num, randomNumber);
                 if(hasWon){
                     break;
@@ -38,9 +41,10 @@ public class NumberGuess {
                 attempts++;
             }catch (InputMismatchException e){
                 System.out.println("Error: 1 to 20 Integers only.");
-                input.nextLine();
+                input.nextLine();  // if user Input mismatched ignore that attempt.
             }
         }
+//        user unable to guess withing 5 attempts.
         if (!hasWon){
             System.out.println("---------------------------------");
             System.out.println("Your attempts over.");
@@ -48,8 +52,9 @@ public class NumberGuess {
         }
     }
 
+//    Method to get the result.
     public static boolean result(int guess, int rand){
-
+//        if user input equals random user won.
         if(guess == rand){
             System.out.println("---------------------------------");
             System.out.println("You won");
@@ -59,7 +64,6 @@ public class NumberGuess {
         }else{
             System.out.println("Your guess is too high.");
         }
-
         return false;
     }
 }
