977579e502dd1ec4264e02847ae8475d1d92fef0
Gaindu Amarasinghe <gaindu2k03@gmail.com>
2025-05-08 16:52:14 +0530

    Number guessing done

--- src/pack/learner/basic/task2/NumberGuess.java
+++ src/pack/learner/basic/task2/NumberGuess.java
@@ -9,7 +9,10 @@ public class NumberGuess {
         int randomNumber = random.nextInt(20) + 1;
         System.out.println(randomNumber);
 
-        while(true){
+        int attempts = 1;
+        boolean hasWon = false;
+
+        while(attempts <= 5){
             Scanner input = new Scanner(System.in);
 
             System.out.print("""
@@ -19,24 +22,37 @@ public class NumberGuess {
             System.out.print("Guess the number: ");
             int guessed_num = input.nextInt();
 
-            if(result(guessed_num, randomNumber)){
+            hasWon = result(guessed_num, randomNumber);
+            if(hasWon){
                 break;
             }
+            attempts++;
         }
-
+        if (!hasWon){
+            System.out.println("---------------------------------");
+            System.out.println("Your attempts over.");
+            System.out.println("Correct Answer: " + randomNumber);
+        }
     }
 
     public static boolean result(int guess, int rand){
-        boolean finished = false;
+
         if(guess == rand){
+            System.out.println("---------------------------------");
             System.out.println("You won");
-            finished = true;
+            return true;
         }else if(guess < rand){
             System.out.println("Your guess is too low.");
         }else{
             System.out.println("Your guess is too high.");
         }
-        
-        return finished;
+
+        return false;
+    }
+
+    public static void validateInput(int number){
+        if (number < 0 || number > 20){
+            System.out.println("Invalid Inp");
+        }
     }
 }
